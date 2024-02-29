f3f093be16eb3f44c5e297bf9291faea26ba1fb2
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-29 06:56:22 -0300

    feat: tuning responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ import { BsThreeDotsVertical } from "react-icons/bs";
 export default function Home() {
   return (
     <div className="m-auto max-w-[1000px] p-8">
-      <div className="mt-6 overflow-x-auto">
+      <div className="mt-6">
         <table className="border divide-y divide-gray-190">
-          <thead className="bg-gray-50">
+          <thead className="sm:hidden bg-gray-50">
             <tr>
               <th className="px-6 py-3 text-left text-xs font-medium text-grady-500 uppercase tracking-wider">
                 car
@@ -28,11 +28,12 @@ export default function Home() {
               </th>
             </tr>
           </thead>
-          <tbody className="bg-white divide-y divide-gray-190 ">
+          <tbody className="bg-white divide-y divide-gray-190">
             {listCars.map((item) => (
               <tr key={item.id}>
                 <td className="flex items-center gap-4 px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                   <Image
+                  className="sm:w-[120px] md:inline"
                     src={item.imageUrl}
                     width={70}
                     height={70}
@@ -40,10 +41,10 @@ export default function Home() {
                   />
                   <span>{item.name}</span>
                 </td>
-                <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
-                  {item.reservation}
+                <td className="sm:flex md:items-center sm:px-1 sm:py-1 px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                 <span>Next reservation:</span> {item.reservation}
                 </td>
-                <td className="px-7 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                <td className="sm:hidden px-7 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                   <span
                     className={
                       item.status
@@ -54,12 +55,12 @@ export default function Home() {
                     {item.status ? "Available" : "Unavailable"}
                   </span>
                 </td>
-                <td className="px-7 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                <td className="sm:flex sm:px-1 sm:pr-10 px-7 sm:py-1 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                   <span>
                     <Ratings totalStars={item.rating} />
                   </span>
                 </td>
-                <td className="px-[3rem] py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                <td className="px-[3rem] sm:px-1 sm:py-0  py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                   <BsThreeDotsVertical />
                 </td>
               </tr>
