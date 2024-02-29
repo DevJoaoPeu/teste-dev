388fa18d519667519c3b40a56525400cf965affe
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-29 00:26:48 -0300

    feat: create component listCars, to list the cars, and do the typing

---
+++ components/ListCars.tsx
@@ -0,0 +1,16 @@
+interface PropsList {
+    id: string;
+    imageUrl: string;
+    reservation: string;
+    status: boolean;
+    rating: number
+}
+
+export const listCars: PropsList[] = [
+  { id: "01", imageUrl: "", reservation: "", status: true, rating: 1 },
+  { id: "02", imageUrl: "", reservation: "", status: true, rating: 1 },
+  { id: "03", imageUrl: "", reservation: "", status: true, rating: 1 },
+  { id: "04", imageUrl: "", reservation: "", status: true, rating: 1 },
+  { id: "05", imageUrl: "", reservation: "", status: true, rating: 1 },
+  { id: "06", imageUrl: "", reservation: "", status: true, rating: 1 }
+];
