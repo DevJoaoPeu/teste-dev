a6665f6a8e642472a004d2c7ea8acfbc59f2e749
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-29 01:01:16 -0300

    feat: adding colors that will be used in the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        "green-300": "#deF7EC",
+        "green-700": "#03543f",
+        "gray-200": "#6b7280"
       },
     },
   },
