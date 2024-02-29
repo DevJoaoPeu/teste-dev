88389b55c767c5b98efa0c04829ff8a3a55c5ee4
DevJoaoPeu <joaocanaldroid@gmail.com>
2024-02-29 05:45:31 -0300

    feat: finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { listCars } from "@/components/ListCars";
 import { Ratings } from "@/components/Rating";
 import Image from "next/image";
+
 import { BsThreeDotsVertical } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <div className="m-auto max-w-[1000px] p-5">
-      <div className="mt-6">
-        <table className="w-full border divide-y divide-gray-190">
+    <div className="m-auto max-w-[1000px] p-8">
+      <div className="mt-6 overflow-x-auto">
+        <table className="border divide-y divide-gray-190">
           <thead className="bg-gray-50">
             <tr>
               <th className="px-6 py-3 text-left text-xs font-medium text-grady-500 uppercase tracking-wider">
@@ -29,7 +30,7 @@ export default function Home() {
           </thead>
           <tbody className="bg-white divide-y divide-gray-190 ">
             {listCars.map((item) => (
-              <tr>
+              <tr key={item.id}>
                 <td className="flex items-center gap-4 px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                   <Image
                     src={item.imageUrl}
@@ -43,9 +44,15 @@ export default function Home() {
                   {item.reservation}
                 </td>
                 <td className="px-7 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
-                    <span className={item.status ? `p-1.5 bg-green-200 text-green-700 rounded-md font-bold`: `p-1.5 bg-red-200 text-red-700 rounded-md font-bold`}>
-                      {item.status ? "Available": "Unavailable"}
-                    </span>
+                  <span
+                    className={
+                      item.status
+                        ? `p-1.5 bg-green-200 text-green-700 rounded-md font-bold`
+                        : `p-1.5 bg-red-200 text-red-700 rounded-md font-bold`
+                    }
+                  >
+                    {item.status ? "Available" : "Unavailable"}
+                  </span>
                 </td>
                 <td className="px-7 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">
                   <span>
